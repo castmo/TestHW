7724efabe3540c3432d970f969381aa1b328dd2f
castmo <thomas.castmo@gmail.com>
2016-09-29 22:55:53 +0200

    time dialog finished

--- app/src/main/java/com/example/ecastho/testhw/MainActivity.java
+++ app/src/main/java/com/example/ecastho/testhw/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ecastho.testhw;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +11,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity
+        extends AppCompatActivity
+        implements View.OnClickListener, TimeSetter.OnTimeSetterFinishedListener {
 
     private Button btnHitIt;
     private TextView textArea;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +37,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textArea = (TextView)findViewById(R.id.textarea);
-        btnHitIt = (Button)findViewById(R.id.button_hitit);
+        textArea = (TextView) findViewById(R.id.textarea);
+        btnHitIt = (Button) findViewById(R.id.button_hitit);
         //btnHitIt.setOnClickListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void onClick(View v) {
@@ -38,13 +55,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //startActivity(intent);
     }
 
-    private Date showSetTimeDialog() {
-        FragmentManager fm = getSupportFragmentManager();
-        TimeSetter timeSet = new TimeSetter();
-        timeSet.show(fm, "fragment_time_setter");
+    private void showSetTimeDialog() {
+
+        Date date = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+
+        try {
+            date = sdf.parse("2016-08-22T22:11:23");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (date != null) {
+            FragmentManager fm = getSupportFragmentManager();
+            TimeSetter timeSet = TimeSetter.newInstance(date);
+            timeSet.SetOnTimeSetterFinishedListener(this);
+            timeSet.show(fm, "fragment_time_setter");
+
+        }
 
         // test
-        return new Date();
+
     }
 
+    @Override
+    public void OnTimeSetterFinished(Date date) {
+        if (date == null)
+        {}
+        else
+        {}
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
--- app/src/main/java/com/example/ecastho/testhw/TimeSetter.java
+++ app/src/main/java/com/example/ecastho/testhw/TimeSetter.java
@@ -1,26 +1,57 @@
 package com.example.ecastho.testhw;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 
 public class TimeSetter extends DialogFragment {
-
-    private EditText mEditText;
     private TimePicker mTimePicker;
+    private DatePicker mDatePicker;
+    private Date mDate;
+    private OnTimeSetterFinishedListener mCallback;
+
+    public interface OnTimeSetterFinishedListener {
+        void OnTimeSetterFinished(Date date);
+    }
+
+    static TimeSetter newInstance(Date date) {
+        TimeSetter ts = new TimeSetter();
+        Bundle args = new Bundle();
+        args.putLong("longdate", date.getTime());
+        ts.setArguments(args);
+        return ts;
+    }
 
     public TimeSetter() {
         // Empty constructor required for DialogFragment
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDate = new Date(getArguments().getLong("longdate"));
+    }
+
+    public void SetOnTimeSetterFinishedListener(OnTimeSetterFinishedListener callback) {
+        mCallback = callback;
     }
 
     @Override
@@ -28,36 +59,79 @@ public class TimeSetter extends DialogFragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_time_setter, container);
         mTimePicker = (TimePicker)view.findViewById(R.id.timePicker);
+        mDatePicker = (DatePicker)view.findViewById(R.id.datePicker);
+        mTimePicker.setIs24HourView(true);
+        setPickers(mDate);
         view.findViewById(R.id.buttonCancel).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                buttonClicked("cancel");
+                buttonCancelClicked();
             }
         });
         view.findViewById(R.id.buttonNow).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                buttonClicked("Now");
+                buttonNowClicked();
             }
         });
         view.findViewById(R.id.buttonOk).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                buttonClicked("Ok");
+                buttonOkClicked();
             }
         });
-        //btn.setOnClickListener((View.OnClickListener) this);
-
-        //mEditText = (EditText) view.findViewById(R.id.txt_your_name);
-        getDialog().setTitle("Hello");
-
+        getDialog().setTitle("");
         return view;
     }
 
-    private void buttonClicked(String s) {
-        if(s=="") {
-            return;
+    private void setPickers(Date date) {
+
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int mon = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int h = calendar.get(Calendar.HOUR_OF_DAY);
+        int min = calendar.get(Calendar.MINUTE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mTimePicker.setHour(h);
+            mTimePicker.setMinute(min);
         }
+        else {
+            mTimePicker.setCurrentHour(h);
+            mTimePicker.setCurrentMinute(min);
+        }
+        mDatePicker.init(year, mon, day, null);
+    }
+    private Date getPickers() {
+        int year = mDatePicker.getYear();
+        int mon = mDatePicker.getMonth();
+        int day = mDatePicker.getDayOfMonth();
+        int h, min;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            h = mTimePicker.getHour();
+            min = mTimePicker.getMinute();
+        }
+        else {
+            h = mTimePicker.getCurrentHour();
+            min = mTimePicker.getCurrentMinute();
+        }
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.set(year, mon, day, h, min);
+        return calendar.getTime();
     }
 
+    private void buttonNowClicked() {
+        setPickers(new Date());
+    }
+    private void buttonOkClicked() {
+        if (mCallback != null) {
+            mCallback.OnTimeSetterFinished(getPickers());
+        }
+        this.dismiss();
+    }
+    private void buttonCancelClicked() {
+        this.dismiss();
+    }
 }
